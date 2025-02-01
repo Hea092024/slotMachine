cdef4dcca8ae9c5b43de0b5a2eccf4482a896b68
Hector Acevedo <HEA092024@gmail.com>
2025-02-01 10:44:18 +0100

    Created deposit function

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-//1. Deposit some money@
-//2. Determine number of lines to bet on 
+//1. Deposit some money - Checked!
+//2. Determine number of lines to bet on
 //3. Collect a bet amount
 //4. Spin the slot machine
 //5. Check if the user won
 //7. Give the user their winnings
 //8. Play again
+
+const prompt = require("prompt-sync")();
+while (true){
+
+const deposit = () => {
+  const depositAmount = prompt("Enter a deposit amount: ");
+  const numberDepositAmount = parseFloat(depositAmount);
+
+  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+    console.log("Invalid deposit amount, try again");
+  } else {
+    return numberDepositAmount;
+  }
+}
+};
+
+deposit();
